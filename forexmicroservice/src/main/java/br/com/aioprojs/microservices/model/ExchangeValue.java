41631b5babce8804de4b121e938f43bb232eec23
Sandro Mendes <josesandromendes@gmail.com>
2019-06-08 22:52:58 -0300

    Ajustes no eurekanamingserver e informando nome da coluna em entidade

--- forexmicroservice/src/main/java/br/com/aioprojs/microservices/model/ExchangeValue.java
+++ forexmicroservice/src/main/java/br/com/aioprojs/microservices/model/ExchangeValue.java
@@ -2,15 +2,26 @@ package br.com.aioprojs.microservices.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class ExchangeValue {
 
+	@Id
+	@GeneratedValue
 	private Long id;
 	
+	@Column(name="currency_from")
 	private String from;
 	
+	@Column(name="currency_to")
 	private String to;
 	
 	private BigDecimal conversionMultiple;
